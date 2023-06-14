54d62df6e84e9f4495d54507b0fcb15032d1f35a
Bello Muhammad <67134404+Bello-Muhammad@users.noreply.github.com>
2023-06-14 13:00:03 +0100

    config added

---
+++ src/backend/config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+// mongoose setup
+mongoose.set('strictQuery', false)
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then((res) => {
+    console.log("mongodb connected");
+});
